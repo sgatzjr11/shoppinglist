df304cdfdbdf6326bb4e3004ebf7be8d251a9582
sgatzjr11 <sgatzjr11@cox.net>
2014-11-09 17:42:16 -0500

    Adding shopping list site

---
+++ js/app.js
@@ -0,0 +1,41 @@
+$(document).ready(function()
+	{
+		$("#add-button").on("click", function(e)
+			{
+				e.preventDefault();
+				addButton();
+			});
+	
+
+		$(".shop-table").on("click", ".fa-trash-o", function(e)
+			{
+				e.preventDefault();
+				$(this).closest("tr").fadeOut("slow", function() 
+				{
+					$(this).remove()
+				});
+				
+			});
+
+
+
+		$(".shop-table").on("click", ".fa-check", function(e)
+			{
+				e.preventDefault();
+				$(this).closest("tr").find("span").toggleClass("cross-out");
+				$(this).toggleClass("red");
+		});
+
+
+});
+
+
+
+    var addButton = function(){
+			var add = $("#add-items").val();
+			var newItem = $("<tr><td class='trash'><i class='fa fa-trash-o fa-2x'></i></td><td class='list-item'><span class='items'>" + add + "</span></td><td class='check'><i class='fa fa-check fa-2x'></i></td></tr>").hide();
+			$(".shop-table").append(newItem);
+			newItem.fadeIn("slow");
+			$("#add-items").val("");
+	}
+
